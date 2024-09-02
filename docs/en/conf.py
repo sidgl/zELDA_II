f36c976a09187d1ae36f6549e96d8f331f609585
Siddhartha Gurung López <sidgurung@mintaka.oauv.uv.es>
2024-09-02 11:12:20 +0200

    docs

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -1,70 +1,28 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../Lya_zelda_II/'))
-#sys.path.insert(0, os.path.abspath('../TEST_doc/'))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'zELDA'
-copyright = '2024, Gurung-Lopez, Siddhartha'
-author = 'Gurung-Lopez, Siddhartha'
-
-# The full version, including alpha/beta/rc tags
+project = 'zELDA_II'
+copyright = '2024, Siddhartha Gurung Lopez'
+author = 'Siddhartha Gurung Lopez'
 release = '0.0.01'
 
-
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.viewcode','sphinx.ext.autosectionlabel']
+extensions = []
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-#html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = 'alabaster'
 html_static_path = ['_static']
-
-html_build_dir = os.environ.get('READTHEDOCS_OUTPUT', 'docs/en/build/html')
-##################################
-##################################
-##################################
-#import sphinx_rtd_theme
-#
-#html_theme = 'sphinx_rtd_theme'
-#
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-#
-html_logo = "figs_and_codes/fig_log_DOUBLE_COOL_SHELL_EDGE_r_0.8_s_40_WHITE_True.png"
-
-
